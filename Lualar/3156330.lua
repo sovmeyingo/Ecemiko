270e4cd80ca20852549ecafaa0a6628804f757da
nefemnin <codebmts@gmail.com>
2026-08-05 02:03:20 +0300

    Add files via upload

---
+++ Lualar/3156330.lua
@@ -0,0 +1,8 @@
+-- Anomaly ​​‌‌​​​‌​​‌‌​‌​‌​​‌‌​‌‌​​‌‌​​​​‌​​‌‌​​‌‌​​‌‌​​‌​​‌‌​​‌​‌​‌‌​​‌​​​‌‌​​‌‌​​​‌‌​​‌​​‌‌​​​‌​​​‌‌​‌‌‌​​‌‌​‌​​​‌‌​​​​‌​‌‌​​‌‌​​​‌‌‌​​‌President
+-- AppID 3156330 | Generated on 2026-08-04 22:58 UTC | openlua.cloud
+
+-- Main Application
+addappid(3156330)
+
+-- Content Depots (1)
+addappid(3156331, 1, "e40d152eead510c87f1b945f16e2a12cdf0b56ec49cf5a58231902cc92db3060") -- Anomaly President - Content
