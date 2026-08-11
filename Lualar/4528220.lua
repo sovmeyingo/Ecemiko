9a8df782afb46792b3900f49e1f2b1bb74ee14d8
nefemnin <codebmts@gmail.com>
2026-08-11 07:02:36 +0300

    Add files via upload

---
+++ Lualar/4528220.lua
@@ -0,0 +1,12 @@
+-- 4528220's Lua and Manifest Created by Hubcap Manifest
+-- MEOWMI
+-- Created: August 10, 2026 at 01:41:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4528220) -- MEOWMI
+-- MAIN APP DEPOTS
+addappid(4528221, 1, "1c00b312f2581b93aa4cd9f0eaa07544d35cedd1071a5a87f0dd8671416bf70f") -- Depot 4528221
+setManifestid(4528221, "1817391353185570011", 8677661729)
