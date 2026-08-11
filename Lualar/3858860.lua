40ad1c95fed81517790f22a1f56b897823e724e7
nefemnin <codebmts@gmail.com>
2026-08-11 06:57:31 +0300

    Add files via upload

---
+++ Lualar/3858860.lua
@@ -0,0 +1,14 @@
+-- 3858860's Lua and Manifest Created by Hubcap Manifest
+-- IKARUS
+-- Created: August 10, 2026 at 23:56:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3858860) -- IKARUS
+-- MAIN APP DEPOTS
+addappid(3858861, 1, "1fa85ae7219a63b03c1712aa0bf071c0d136f77a33c8812a4eaafeb6f3da044e") -- Depot 3858861
+setManifestid(3858861, "1587465097088951268", 524880623)
+addappid(3858862, 1, "7e08075ebd8145849f8dbffd4449db9340cc9e64d166d83df63c95a4613d1971") -- Depot 3858862
+setManifestid(3858862, "8800867481720638417", 530170356)
