16dd8486bfbbfde15b7092ff06da2f2b4ae9af2b
nefemnin <codebmts@gmail.com>
2026-08-16 17:16:38 +0300

    Add files via upload

---
+++ Lualar/3818020.lua
@@ -0,0 +1,12 @@
+-- 3818020's Lua and Manifest Created by Hubcap Manifest
+-- Evil School
+-- Created: August 14, 2026 at 07:41:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3818020) -- Evil School
+-- MAIN APP DEPOTS
+addappid(3818021, 1, "3c3bea1d791631b31077c7668fc448570f9862cc9659dbf5cd10e829859b853f") -- Depot 3818021
+setManifestid(3818021, "7897883800208582230", 25274657340)
