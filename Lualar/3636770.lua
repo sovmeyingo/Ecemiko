c3cdc6ae15592b3c74c94a0bc2433c6e2add6ec4
nefemnin <codebmts@gmail.com>
2026-08-16 20:01:11 +0300

    Add files via upload

---
+++ Lualar/3636770.lua
@@ -0,0 +1,14 @@
+-- 3636770's Lua and Manifest Created by Hubcap Manifest
+-- Servant of the Lake
+-- Created: August 14, 2026 at 12:19:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3636770, 1, "1dc4585b4e63a97e168470a4cfcf2812c60219abfe7d544c332fb08ace7d7141") -- Servant of the Lake
+-- MAIN APP DEPOTS
+addappid(3636773, 1, "47efd8d9e4421b857f586ee20c067790f4360b03d9b21e7137ceb751d0c6d638") -- Depot 3636773
+setManifestid(3636773, "4933598770530204050", 713917210)
+addappid(3636774, 1, "6dde3b2c075c250b5d135902338e5991ce3da4992f204d3e3140821d89390c5a") -- Depot 3636774
+setManifestid(3636774, "1777860244140010682", 760122934)
