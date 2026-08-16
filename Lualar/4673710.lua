1eaa67e069595887cce228c3339fd37ac6c25b37
nefemnin <codebmts@gmail.com>
2026-08-16 21:02:29 +0300

    Add files via upload

---
+++ Lualar/4673710.lua
@@ -0,0 +1,12 @@
+-- 4673710's Lua and Manifest Created by Hubcap Manifest
+-- Infinite Backrooms
+-- Created: August 15, 2026 at 03:08:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4673710) -- Infinite Backrooms
+-- MAIN APP DEPOTS
+addappid(4673711, 1, "9cac91f6095231e1d5c3f7e8e5b3ff030166fd5ea1e431ab5b73a1ccc7465727") -- Depot 4673711
+setManifestid(4673711, "7348913838669166642", 1383743720)
