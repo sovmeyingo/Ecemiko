be2ded1d794133e8320c429e4f184a537dbd987f
nefemnin <codebmts@gmail.com>
2026-08-16 19:42:30 +0300

    Add files via upload

---
+++ Lualar/4934370.lua
@@ -0,0 +1,20 @@
+-- 4934370's Lua and Manifest Created by Hubcap Manifest
+-- TV Archive: Tidy Up Together
+-- Created: August 16, 2026 at 07:30:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(4934370, 1, "83adf31d4c8d9e04213146687dea7027d0ae573147405f1f141333cccded7446") -- TV Archive: Tidy Up Together
+-- MAIN APP DEPOTS
+addappid(4934371, 1, "c154d948768afc66d1a7b432e8c2b82dfb40a677e322acf34935db406dba8488") -- Depot 4934371
+setManifestid(4934371, "5128235383296166454", 2370610024)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
