8d0698653cfe3933dd8d1cb0bca29d59b2c66649
nefemnin <codebmts@gmail.com>
2026-08-16 19:34:08 +0300

    Add files via upload

---
+++ Lualar/3091140.lua
@@ -0,0 +1,16 @@
+-- 3091140's Lua and Manifest Created by Hubcap Manifest
+-- Lootbound
+-- Created: August 15, 2026 at 10:49:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3091140, 1, "1f05d6f3ddade4323eb34249f78ed3edae22a7b8d73bbf70c1ca4165965e54ca") -- Lootbound
+-- MAIN APP DEPOTS
+addappid(3091141, 1, "bfd0833e4e531ac8033b18ae9b79eea5b2ee46b29b62f7959f97caec7547be1c") -- Depot 3091141
+setManifestid(3091141, "7795847844861089497", 633581928)
+addappid(3091142, 1, "487ae9dc13c277989189e32614056f9fc545ad6860e154673c32dfd568b470de") -- Depot 3091142
+setManifestid(3091142, "1644998472010110015", 970665460)
+addappid(3091143, 1, "9f32af5e3cc8ccf9bea79d4f5a5f09f8de74c727d5f64b023cdc40116dbbb9dd") -- Depot 3091143
+setManifestid(3091143, "6970850558717887277", 688114397)
