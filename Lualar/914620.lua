67e78a52cd6c95d92a1d72693b7951a952a5ef22
nefemnin <codebmts@gmail.com>
2026-08-16 20:43:05 +0300

    Add files via upload

--- Lualar/914620.lua
+++ Lualar/914620.lua
@@ -1,2 +1,18 @@
-addappid(914620)
-addappid(914621,0,"3de6377dcc0c7e93bb1fdd7a9b8b9f13d72ab35f016e283d585b3a595f8b6367")
+-- 914620's Lua and Manifest Created by Hubcap Manifest
+-- Mist Survival
+-- Created: November 15, 2025 at 00:32:55 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(914620) -- Mist Survival
+-- MAIN APP DEPOTS
+addappid(914621, 1, "3de6377dcc0c7e93bb1fdd7a9b8b9f13d72ab35f016e283d585b3a595f8b6367") -- Mist Survival Content
+setManifestid(914621, "7268530621364451212", 28615802536)
+-- SHARED DEPOTS (from other apps)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+setManifestid(228986, "8782296191957114623", 29759921)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
