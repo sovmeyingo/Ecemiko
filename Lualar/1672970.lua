ee6d7f136a28c720aafe01464e30ab67741b014f
nefemnin <codebmts@gmail.com>
2026-08-20 20:16:30 +0300

    Add files via upload

--- Lualar/1672970.lua
+++ Lualar/1672970.lua
@@ -1,14 +1,34 @@
-addappid(1672970)
-addappid(1672971,0,"b42295e44d98b2ed35338c9f96b4c65ad99a839023e9e4ddef8e19ddc7addac4")
-addappid(1776550)
-addappid(1672980)
-addappid(1672981)
-addappid(1672982)
-addappid(1672983)
-addappid(1672984)
-addappid(1672985)
-addappid(1672986)
-addappid(1717450)
+-- 1672970's Lua and Manifest Created by Hubcap Manifest
+-- Minecraft Dungeons
+-- Created: September 30, 2025 at 00:54:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 11
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1672970) -- Minecraft Dungeons
+-- MAIN APP DEPOTS
+addappid(1672971, 1, "b42295e44d98b2ed35338c9f96b4c65ad99a839023e9e4ddef8e19ddc7addac4") -- Lovika Content
+setManifestid(1672971, "4325055371153456789", 5816003899)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Minecraft Dungeons Ultimate Edition Digital Artwork (AppID: 1726570)
 addappid(1726570)
-addappid(1850660)
-addappid(1979530)
+addappid(1726570, 1, "0e89af5042b93fdac6bf07b8155a9addf8bc19a556fed00a326ece6e7c9e7a14") -- Minecraft Dungeons Ultimate Edition Digital Artwork - Depot 1726570
+setManifestid(1726570, "2582615933734748879", 43513432)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1672980) -- Minecraft Dungeons Jungle Awakens
+addappid(1672981) -- Minecraft Dungeons Creeping Winter
+addappid(1672982) -- Minecraft Dungeons Howling Peaks
+addappid(1672983) -- Minecraft Dungeons Flames of the Nether
+addappid(1672984) -- Minecraft Dungeons Hidden Depths
+addappid(1672985) -- Minecraft Dungeons Echoing Void
+addappid(1672986) -- Minecraft Dungeons Hero DLC
+addappid(1776550) -- Minecraft Dungeons Cloudy Climb Adventure Pass
+addappid(1850660) -- Minecraft Dungeons Luminous Night Adventure Pass
+addappid(1979530) -- Minecraft Dungeons Fauna Faire Adventure Pass
--- Lualar/1717450.lua
+++ Lualar/1717450.lua
@@ -1,3 +1,14 @@
-addappid(1717450)
-addappid(1717451,0,"537a4254a9eb6f712e387077aea3440baed45fecc5d77804bbae0186e98ba92b")
-addappid(1717452,0,"e24b4a2f0fc286cc4d2c8589889bccdf8735e4b9f76d4ee5bd9200e78baf25d0")
+-- 1717450's Lua and Manifest Created by Hubcap Manifest
+-- Minecraft Dungeons Ultimate Edition Soundtrack
+-- Created: September 30, 2025 at 00:55:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1717450) -- Minecraft Dungeons Ultimate Edition Soundtrack
+-- MAIN APP DEPOTS
+addappid(1717451, 1, "537a4254a9eb6f712e387077aea3440baed45fecc5d77804bbae0186e98ba92b") -- Lovika Soundtrack Content
+setManifestid(1717451, "1493749821264086954", 1690558925)
+addappid(1717452, 1, "e24b4a2f0fc286cc4d2c8589889bccdf8735e4b9f76d4ee5bd9200e78baf25d0") -- Lovika Soundtrack FLAC
+setManifestid(1717452, "1157092614209266083", 7436609342)
