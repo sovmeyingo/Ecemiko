d1b220f773f72cae73b93036dd786d47a476a125
nefemnin <codebmts@gmail.com>
2026-08-20 20:09:18 +0300

    Add files via upload

---
+++ Lualar/2584270.lua
@@ -0,0 +1,21 @@
+-- 2584270's Lua and Manifest Created by Hubcap Manifest
+-- Mortal Shell II
+-- Created: August 20, 2026 at 13:05:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 2
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2584270, 1, "2b9f4a2124d5757f3d4f46c81c6d4082f3b1d1ac9b3233acf41ddc6b5c15810a") -- Mortal Shell II
+-- MAIN APP DEPOTS
+addappid(2584271, 1, "bb59e6dc30df8728a4bfcc3129ca9b8284ed3808d42a9a272e65d4904b4bc445") -- Depot 2584271
+setManifestid(2584271, "5187269532406368119", 73091542733)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4711720) -- Mortal Shell II Devout Edition Upgrade
+addtoken(4711720, "14235145898889595086")
+addappid(4711730) -- Mortal Shell II - Pre-order Bonus
+addtoken(4711730, "2594855405444440804")
