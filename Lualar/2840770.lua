d896d6ff93d3b027e6e1ce060818d271f5230036
nefemnin <codebmts@gmail.com>
2026-08-22 18:57:57 +0300

    Add files via upload

--- Lualar/2840770.lua
+++ Lualar/2840770.lua
@@ -1,9 +1,62 @@
-addappid(2840770)
-addappid(2840771,0,"64292a119e4b390ef4488dd942329a7794234989b74c79e3228adb22bfd9d4e9")
-addappid(1716751,0,"84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
-addappid(2840772,0,"00c6fe38e74b05f6dc70d62fd74a6e8f4eeb91d9e7b9db53368a991173c9e10a")
-addappid(2840773,0,"318c6390e6722d953e5d87462e9ef7b523aaae813fc3e4fdb0b8c409217ba832")
-addappid(2840774,0,"9b994fdd7426be8a12fccb94c5e7f0fa17467a0f210941d82c54e2308d547853")
-addappid(2840775,0,"119fce59e34c3146f02c4d5177801f6262085810b15b230bde3e59beb8510737")
-addappid(3005010,0,"3fefd54093821f0ae2793878338384715c8b4061d70ae3a04e4fb281684c0af0")
-addappid(3005000,0,"bb5b5446892c4e6b050d88e7e41f1d775f39970ea3d5998380d59084bcd5f5d4")
+-- Generated with Luie @ https://lua.tools/
+-- 2840770 - Avatar: Frontiers of Pandora
+-- Version-locked to Build 22429549 (No title) — released 2026-03-24 08:03:05 UTC
+-- Generated 2026-08-03 23:52:38 UTC
+-- # Depots (Total/DLC/Shared): 11/3/3
+
+-- Main AppID
+addappid(2840770, 1, "80d5aea0635722558bcc838ada07e6e938c2801221cf525f8bf21b9311f47381")
+
+-- Main Depots
+addappid(2840771, 1, "64292a119e4b390ef4488dd942329a7794234989b74c79e3228adb22bfd9d4e9") -- (windows)
+setManifestid(2840771, "1313257464760795001")
+addappid(2840772, 1, "00c6fe38e74b05f6dc70d62fd74a6e8f4eeb91d9e7b9db53368a991173c9e10a") -- French
+setManifestid(2840772, "591329598511062425")
+addappid(2840773, 1, "318c6390e6722d953e5d87462e9ef7b523aaae813fc3e4fdb0b8c409217ba832") -- German
+setManifestid(2840773, "1685669709126676964")
+addappid(2840774, 1, "9b994fdd7426be8a12fccb94c5e7f0fa17467a0f210941d82c54e2308d547853") -- Spanish
+setManifestid(2840774, "6925198600522292954")
+addappid(2840775, 1, "119fce59e34c3146f02c4d5177801f6262085810b15b230bde3e59beb8510737") -- Brazilian
+setManifestid(2840775, "4119199569999406077")
+
+-- DLC's (no depot keys required)
+addappid(2857020) -- Avatar: Frontiers of Pandora DELUXE Ubisoft Activation
+addappid(2857030) -- Avatar: Frontiers of Pandora GOLD Ubisoft Activation
+addappid(2857040) -- Avatar: Frontiers of Pandora ULTIMATE Ubisoft Activation
+addappid(2857260) -- Avatar: Frontiers of Pandora™ – Season Pass
+addappid(2857270) -- Avatar: Frontiers of Pandora  Season Pass Ubisoft Activation
+addappid(2857300) -- Sky Rider Starter Pack – Avatar: Frontiers of Pandora™
+addappid(2857310) -- Avatar: Frontiers of Pandora Starter Pack Ubisoft Activation
+addappid(2857390) -- Avatar: Frontiers of Pandora Ubisoft Activation
+addappid(3087130) -- Avatar: Frontiers of Pandora The Sky Breaker DLC Ubisoft Activation
+addappid(3228830) -- Avatar: Frontiers of Pandora Secrets of the Spires DLC Ubisoft Activation
+addappid(3231160) -- Storm Chaser Starter Pack - Avatar: Frontiers of Pandora™
+addappid(3231170) -- Avatar: Frontiers of Pandora - Storm Chaser Starter Pack Ubisoft Activation
+addappid(3830230) -- From The Ashes Expansion - Avatar: Frontiers of Pandora™ Ubisoft Activation
+addappid(3914600) -- JAKE SULLY TORUK MAKTO STARTER PACK - AVATAR: FRONTIERS OF PANDORA™
+addappid(3914610) -- Avatar: Frontiers of Pandora - Starter pack - Jake Sully Toruk Makto Ubisoft Activation
+addappid(3914620) -- VALLEY OF MO’ARA STARTER PACK - AVATAR: FRONTIERS OF PANDORA™
+addappid(3914630) -- Avatar: Frontiers of Pandora - Valley of Mo'ara Starter pack Ubisoft Activation
+addappid(3914640) -- Avatar: Frontiers of Pandora From the Ashes Edition Ubisoft Activation
+addappid(3914650) -- Avatar: Frontiers of Pandora Complete Edition Ubisoft Activation
+addappid(3915910) -- From The Ashes Expansion - Avatar: Frontiers of Pandora™
+addappid(3915920) -- Avatar: Frontiers of Pandora - Preorder DLC Maximus  - Ubisoft Activation
+
+-- DLC's (with depot keys)
+-- Avatar: Frontiers of Pandora™ – The Sky Breaker (AppID: 3005000)
+addappid(3005000)
+addappid(3005000, 1, "bb5b5446892c4e6b050d88e7e41f1d775f39970ea3d5998380d59084bcd5f5d4") -- (windows, 64-bit)
+setManifestid(3005000, "8047920361918511067")
+-- Avatar: Frontiers of Pandora™ – Secrets of The Spire (AppID: 3005010)
+addappid(3005010)
+addappid(3005010, 1, "3fefd54093821f0ae2793878338384715c8b4061d70ae3a04e4fb281684c0af0") -- (windows, 64-bit)
+setManifestid(3005010, "7571111406504859758")
+-- From The Ashes Expansion - Avatar: Frontiers of Pandora™ (AppID: 3823590)
+addappid(3823590)
+addappid(3823590, 1, "c19fc428ff111dd82c4f076e4c19c65ee8377a1b5e1d945d6040cdbcb85e49b5") -- (windows, 64-bit)
+setManifestid(3823590, "7216827091719275431")
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
--- Lualar/3321460.lua
+++ Lualar/3321460.lua
@@ -1,8 +1,32 @@
-addappid(3321460)
-addappid(4024620)
-addappid(4024630)
-addappid(4193060)
-addappid(3321461,0,"e2e570c84eb8b4cfe9c20268cd30b84f8c1c7c076691c92ae8c5ba7b510b8a95")
--- setManifestid(3321461,"8432047928063861989")
-addappid(3321462,0,"6eb3bfe7d16b9eb6441dba4f7d18b341fe83ab2f1e9385d042ce622f38a600cb")
--- setManifestid(3321462,"2375676125334995849")
+-- Generated with Luie @ https://lua.tools/
+-- 3321460 - Crimson Desert
+-- Version-locked to Build 24236217 (Patch Notes Version 1.14.00) — released 2026-07-16 09:06:07 UTC
+-- Generated 2026-08-04 01:36:22 UTC
+-- # Depots (Total/DLC/Shared): 6/3/1
+
+-- Main AppID
+addappid(3321460, 1, "dc014470e47064263f3003feb526a87cc64784292e2a608455d18fa70ec2c1e0")
+
+-- Main Depots
+addappid(3321461, 1, "e2e570c84eb8b4cfe9c20268cd30b84f8c1c7c076691c92ae8c5ba7b510b8a95") -- (windows, 64-bit)
+setManifestid(3321461, "3181503578355214830")
+addappid(3321462, 1, "6eb3bfe7d16b9eb6441dba4f7d18b341fe83ab2f1e9385d042ce622f38a600cb") -- (macos, 64-bit)
+setManifestid(3321462, "5054339860124800981")
+
+-- DLC's (no depot keys required)
+addappid(4024620) -- Crimson Desert - Deluxe Pack
+
+-- DLC's (with depot keys)
+-- Crimson Desert Original Soundtrack Volume 1 (AppID: 4572870)
+addappid(4572870)
+addappid(4572871, 1, "fd5557767303d9a63b0b31b6a22cd1368e5d82a692e64d041890100ea0c330c4")
+setManifestid(4572871, "6173128065949955413")
+addappid(4572872, 1, "e071efcdac5de6bf1b6620a3110cb3ab7799943ee634eed2970db8732eeb817e")
+setManifestid(4572872, "1021886354207583209")
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4783050
+
+-- Token-locked DLCs (couldn't read depots): 4024630, 4193060
